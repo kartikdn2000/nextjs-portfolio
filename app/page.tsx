1e8d6ecde404c8a697643ba83e47f9adf0b1b9a1
Kartik Nayak <kartikdn2000@gmail.com>
2024-12-03 23:44:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { ParticlesBackground } from "@/components/3d/particles-background"
+
 import { HeroContent } from "@/components/home/hero-content"
 
 export default function Home() {
   return (
     <>
-      <ParticlesBackground />
+    
       <div className="container mx-auto px-4 py-16">
         <HeroContent />
       </div>
